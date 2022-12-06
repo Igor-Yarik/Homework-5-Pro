ebf853bb76daafa76f3a9e3525c602285efba2a7
”Igor <igor.yarik@gmail.com>
2022-12-06 17:47:28 +0200

    +

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
-for (let i = 20; i <= 30; i+=0.5) {
+/* for (let i = 20; i <= 30; i+=0.5) {
  console.log(i);
-    } 
+    }  */
+
+/* let dollar = 27
+
+for (let n = 10; n <= 100; n+=10) { 
+   console.log(dollar*n)
+} */
+
+let n = 70
+
+Должны быть числа 1 / 4 / 9 / 16 / 25 / 36 / 49 / 64
